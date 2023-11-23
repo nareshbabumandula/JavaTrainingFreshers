4de11bf89243667450070a0ac490dba0a89751d4
nareshbabumandula <naresh223@gmail.com>
2023-11-23 19:21:47 +0530

    Static methods

--- JavaTrainingBatch/src/com/practice/Arithmatic.java
+++ JavaTrainingBatch/src/com/practice/Arithmatic.java
@@ -2,22 +2,25 @@ package com.practice;
 
 public class Arithmatic {
 	
-	int a=10;
-	int b=20;
+	static int a=10;
+	static int b=20;
 	
 	public void addition() {
 		System.out.println("Addition of a and b is :  " + (a+b));
 	}
+	
 	public void subtraction() {
 		System.out.println("Subtraction of a and b is :  " + (a-b));
 	}
 	public void multiplication() {
 		System.out.println("Multiplication of a and b is :  " + (a*b));
 	}
+	
 	public void division() {
 		System.out.println("Division of a and b is :  " + (b/a));
 	}
-	public void modulus() {
+	
+	public static void modulus() {
 		System.out.println("Modules of a and b is :  " + (b%a));
 	}
 
@@ -27,7 +30,7 @@ public class Arithmatic {
 		arth.subtraction();
 		arth.multiplication();
 		arth.division();
-		arth.modulus();
+		modulus();
 
 	}
 
