fcf65bd4f10b48788f678c92976f6ab54becb7b5
nareshbabumandula <naresh223@gmail.com>
2023-12-08 18:28:05 +0530

    Abstract class

---
+++ JavaTrainingBatch/src/com/oops/Theatre.java
@@ -0,0 +1,32 @@
+package com.oops;
+
+public abstract class Theatre {
+	
+	/*
+	 * 1. Screen size 
+	 * 2. No of Seats 
+	 * 3. Parking 
+	 * 4. Sound system 
+	 * 5. Online Booking Facility 
+	 * 6. Washrooms 
+	 * 7. Canteen 
+	 * 8. Ticket validation 
+	 * 9. Fire and Safety 
+	 * 10.Ticket counter
+	 */
+	
+	// Concrete java method
+	public void OnlineBookingFacility() {
+		// implementation
+		System.out.println("Good to have Online Booking Facility..!");
+	}
+	
+	// abstract or method without body or method definition
+	public abstract void noofSeats();
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
