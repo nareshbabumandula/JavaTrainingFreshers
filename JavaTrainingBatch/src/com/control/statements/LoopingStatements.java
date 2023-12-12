825e80b323a5e1d1ec101d3d52c2905c4e922d1d
nareshbabumandula <naresh223@gmail.com>
2023-12-12 16:58:42 +0530

    12-Dec-2023: Naresh: 1 to n prime numbers program

--- JavaTrainingBatch/src/com/control/statements/LoopingStatements.java
+++ JavaTrainingBatch/src/com/control/statements/LoopingStatements.java
@@ -14,18 +14,18 @@ public class LoopingStatements{
 			System.out.println(i);
 		}
 	}
-	
+
 	public static void infiniteForLoop() {
 		int i=1;
 		for(;;) {
 			System.out.println(i);
 		}
 	}
-	
+
 	public static void verifyPrimeNumber(int num) {
 		int mid=num/2;
 		boolean isPrime=true;
-		
+
 		if(num==0 || num==1) {
 			System.out.println(num + " is not a prime number");
 		}else {
@@ -37,10 +37,30 @@ public class LoopingStatements{
 				}
 			}
 		}
-		
+
 		if(isPrime) {
 			System.out.println(num + " is a prime number");
 		}
+	}
+
+
+	public static void verifyPrimeNumbers(int n) {
+		int i=0;
+		int num=0;
+		String primeNumbers = "";
+
+		for (i = 1; i <= n; i++) { 		  	  
+			int counter=0; 	  
+			for(num=i; num>=1; num--) {
+				if(i%num==0){
+					counter = counter + 1;
+				}
+			}
+			if (counter==2){
+				primeNumbers = primeNumbers + i + " ";
+			}	
+		}	
+		System.out.println(primeNumbers);
 	}
 
 	public static void main(String[] args) {
@@ -48,11 +68,11 @@ public class LoopingStatements{
 		//infiniteForLoop();
 		//verifyPrimeNumber(0);
 		//verifyPrimeNumber(1);
-		verifyPrimeNumber(9);
+		//verifyPrimeNumber(9);
 		//verifyPrimeNumber(15);
 		//verifyPrimeNumber(16);
 		//verifyPrimeNumber(17);
-
+		verifyPrimeNumbers(100);
 	}
 
 }
