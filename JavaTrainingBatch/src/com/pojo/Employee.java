7d9a75bc2cab4b9c4d9cbc1e02a6676c31e0365f
nareshbabumandula <naresh223@gmail.com>
2024-03-21 20:29:11 +0530

    21-Mar-2024: Naresh: Employee pojo class

---
+++ JavaTrainingBatch/src/com/pojo/Employee.java
@@ -0,0 +1,53 @@
+package com.pojo;
+
+public class Employee {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String department;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firsstName=" + firstName + ", lastName=" + lastName + ", department="
+				+ department + "]";
+	}
+	
+	public Employee(int id, String firstName, String lastName, String department) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.department = department;
+	}
+	
+	
+	
+
+	
+}
